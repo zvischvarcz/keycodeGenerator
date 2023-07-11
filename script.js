25ee48e6b067308bde1a0c9fcf64b0320a7d6508
Zvi Schvarcz <zvischvarcz@gmail.com>
2023-07-11 14:01:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const infoBox1 = document.getElementById("infoBox1")
+const infoBox2 = document.getElementById("infoBox2")
+const infoBox3 = document.getElementById("infoBox3")
+const headingContainer = document.getElementById("headingContainer")
+const infoBoxesContainer = document.getElementById("infoBoxesContainer")
+
+
+
+
+
+
+
+
+document.addEventListener("keypress", (event) => {
+    let keycode = event.code;
+    let keyCodeKey = event.key;
+    let keyCodeCharCode = event.charCode;
+    headingContainer.style.display = "none";
+    infoBoxesContainer.style.display = "flex";
+    infoBox1.innerHTML = `<P class="box-title">The event.code is:</p><p class="result-text">${keycode}</p>`
+    infoBox2.innerHTML = `<P class="box-title">The event.key is:</p><p class="result-text">${keyCodeKey}</p>`
+    infoBox3.innerHTML = `<P class="box-title">The event.charCode is:</p><p class="result-text">${keyCodeCharCode}</p>`
+})

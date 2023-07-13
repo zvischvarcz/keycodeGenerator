12e01a2cc416f8afdc2d52a1461a9523b335a597
Zvi Schvarcz <zvischvarcz@gmail.com>
2023-07-13 16:34:20 +0100

    added mobile search bar

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const infoBox2 = document.getElementById("infoBox2")
 const infoBox3 = document.getElementById("infoBox3")
 const headingContainer = document.getElementById("headingContainer")
 const infoBoxesContainer = document.getElementById("infoBoxesContainer")
+const input = document.getElementById("input")
+const inputBtn = document.getElementById("inputBtn")
 
 
 
@@ -15,6 +17,17 @@ document.addEventListener("keypress", (event) => {
     let keycode = event.code;
     let keyCodeKey = event.key;
     let keyCodeCharCode = event.charCode;
+    headingContainer.style.display = "none";
+    infoBoxesContainer.style.display = "flex";
+    infoBox1.innerHTML = `<P class="box-title">The event.code is:</p><p class="result-text">${keycode}</p>`
+    infoBox2.innerHTML = `<P class="box-title">The event.key is:</p><p class="result-text">${keyCodeKey}</p>`
+    infoBox3.innerHTML = `<P class="box-title">The event.charCode is:</p><p class="result-text">${keyCodeCharCode}</p>`
+})
+
+inputBtn.addEventListener("click", () => {
+    let keycode = input.value.code;
+    let keyCodeKey = input.value.key;
+    let keyCodeCharCode = input.value.charCode;
     headingContainer.style.display = "none";
     infoBoxesContainer.style.display = "flex";
     infoBox1.innerHTML = `<P class="box-title">The event.code is:</p><p class="result-text">${keycode}</p>`
